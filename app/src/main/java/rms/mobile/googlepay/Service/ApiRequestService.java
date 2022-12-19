889e84e170d3ba373752fd43659fa5c6e32cab2f
massodasuki <massodasuki@gmail.com>
2022-12-19 14:45:35 +0800

    post

--- app/src/main/java/rms/mobile/googlepay/Service/ApiRequestService.java
+++ app/src/main/java/rms/mobile/googlepay/Service/ApiRequestService.java
@@ -82,7 +82,7 @@ public class ApiRequestService {
                 endPoint = Development.API_PAYMENT + "RMS/API/Direct/1.4.0/index.php";
             }
 
-            endPoint = "https://dummyjson.com/posts";
+            endPoint = "https://dummy.restapiexample.com/api/v1/create";
 
             Uri uri = Uri.parse(endPoint)
                     .buildUpon()
