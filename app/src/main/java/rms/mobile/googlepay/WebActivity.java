d25eef4ebd081528281e7c328a5879b4af85bb70
massodasuki <massodasuki@gmail.com>
2022-12-20 20:10:16 +0800

    add webactivity

--- app/src/main/java/rms/mobile/googlepay/WebActivity.java
+++ app/src/main/java/rms/mobile/googlepay/WebActivity.java
@@ -1,8 +1,19 @@
 package rms.mobile.googlepay;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import rms.mobile.googlepay.Helper.RMSGooglePay;
 
 public class WebActivity extends AppCompatActivity {
 
@@ -10,5 +21,50 @@ public class WebActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        Intent intent = getIntent();
+        String paymentInput = intent.getStringExtra("paymentInput");
+        String paymentInfo = intent.getStringExtra("paymentInfo");
+        Log.d(TAG, String.format("paymentInput: %s - paymentInfo - %s", paymentInput, paymentInfo));
+
+        PaymentTaskRunner runner = new PaymentTaskRunner();
+        runner.execute(paymentInput, paymentInfo);
+
+    }
+    
+    private static class PaymentTaskRunner extends AsyncTask<String, String, String> {
+
+        private String resp;
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                RMSGooglePay pay = new RMSGooglePay();
+                JSONObject result = (JSONObject) pay.requestPayment(
+                        params[0],
+                        params[1]
+                );
+                Log.i("What is in here", "String.valueOf(result)");
+                resp = result.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            Log.i("PaymentTaskRunner doInBackground", resp);
+            return resp;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("PaymentTaskRunner onPostExecute", result);
+//            processValue(result);
+        }
+        @Override
+        protected void onPreExecute() {
+            Log.i("PaymentTaskRunner onPreExecute", "preExecute");
+        }
+        @Override
+        protected void onProgressUpdate(String... text) {
+            Log.e("PaymentTaskRunner onProgressUpdate", "progressUpdate");
+        }
     }
 }
