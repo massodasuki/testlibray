efde859c5309f0874515154af10e149ca9ac55e2
massodasuki <massodasuki@gmail.com>
2022-12-20 15:22:10 +0800

    add main activity

--- app/src/main/java/rms/mobile/googlepay/MainActivity.java
+++ app/src/main/java/rms/mobile/googlepay/MainActivity.java
@@ -1,20 +1,75 @@
 package rms.mobile.googlepay;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import rms.mobile.googlepay.Helper.RMSGooglePay;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    void processValue(String myValue) {
+        //handle value
+        //Update GUI, show toast, etc..
+        Toast.makeText(
+                this, getString(R.string.payments_show_name, myValue),
+                Toast.LENGTH_LONG).show();
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        String url = "http://www.example.com";
+
+        String postData = null;
+        try {
+            postData = "username=" + URLEncoder.encode("my_username", "UTF-8") + "&password=" + URLEncoder.encode("my_password", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        webview.postUrl(url,postData.getBytes());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        String paymentInput = intent.getStringExtra("paymentInput");
+        String paymentInfo = intent.getStringExtra("paymentInfo");
+        Log.d(TAG, String.format("paymentInput: %s - paymentInfo - %s", paymentInput, paymentInfo));
+
+//        JSONObject paymentInputObj = new JSONObject();
+//
+//        try {
+//        paymentInputObj.put("orderId", "order111");
+//        paymentInputObj.put("amount", "1.10");
+//        paymentInputObj.put("currency", "MYR");
+//        paymentInputObj.put("billName", "Abd Qayyum");
+//        paymentInputObj.put("billEmail", "qayyum.ishak@razer.com");
+//        paymentInputObj.put("billPhone", "60133833895");
+//        paymentInputObj.put("billDesc", "Google Pay Testing");
+//        paymentInputObj.put("merchantId", "rmsxdk_mobile_Dev");
+//        paymentInputObj.put("verificationKey", "ee738b541eff7b6b495e44771f71c0ec");
+//        paymentInputObj.put("isSandbox", "false");
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
         // res.body = {
         //     MerchantID: 'razerparcelplus_Dev',
         //     ReferenceNo: 'DDA-887fc953-faaf-4371-9142-dcbd3599c6981557197125028a46555',
@@ -53,20 +108,46 @@ public class MainActivity extends AppCompatActivity {
         //     }
         // }
 
-        WebView webview = new WebView(this);
-        setContentView(webview);
+        PaymentTaskRunner runner = new PaymentTaskRunner();
+        runner.execute(paymentInput, paymentInfo);
 
-        String url = "http://www.example.com";
+    }
 
-        String postData = null;
-        try {
-            postData = "username=" + URLEncoder.encode("my_username", "UTF-8") + "&password=" + URLEncoder.encode("my_password", "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+    private class PaymentTaskRunner extends AsyncTask<String, String, String> {
+
+        private String resp;
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                RMSGooglePay pay = new RMSGooglePay();
+                JSONObject result = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    result = (JSONObject) pay.requestPayment(
+                            params[0],
+                            params[1]
+                    );
+                }
+                Log.i("What is in here", String.valueOf(result));
+                resp = result.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            Log.i("PaymentTaskRunner doInBackground", resp);
+            return resp;
         }
-        webview.postUrl(url,postData.getBytes());
-
-
-
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("PaymentTaskRunner onPostExecute", "Done");
+            processValue(result);
+        }
+        @Override
+        protected void onPreExecute() {
+            Log.i("PaymentTaskRunner onPreExecute", "preExecute");
+        }
+        @Override
+        protected void onProgressUpdate(String... text) {
+            Log.e("PaymentTaskRunner onProgressUpdate", "progressUpdate");
+        }
     }
 }
