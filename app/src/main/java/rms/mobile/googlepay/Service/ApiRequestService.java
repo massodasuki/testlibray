7f477855eed1b2b4ba157b37eb97b7e3287f0cce
massodasuki <massodasuki@gmail.com>
2023-01-11 02:44:40 +0800

    add amount

--- app/src/main/java/rms/mobile/googlepay/Service/ApiRequestService.java
+++ app/src/main/java/rms/mobile/googlepay/Service/ApiRequestService.java
@@ -158,20 +158,20 @@ public class ApiRequestService {
 //                    mMobileSDKParam.getAmount()
 //            );
 
-//            Uri.Builder builder = new Uri.Builder()
-//                    .appendQueryParameter("amount", amount)
-//                    .appendQueryParameter("txID", txID)
-//                    .appendQueryParameter("domain", merchantId)
-//                    .appendQueryParameter("skey", sKey)
-//                    .appendQueryParameter("url", "")
-//                    .appendQueryParameter("type", "0");
-
             Uri.Builder builder = new Uri.Builder()
+                    .appendQueryParameter("amount", amount)
                     .appendQueryParameter("txID", txID)
                     .appendQueryParameter("domain", merchantId)
                     .appendQueryParameter("skey", sKey)
                     .appendQueryParameter("url", "")
                     .appendQueryParameter("type", "0");
+
+//            Uri.Builder builder = new Uri.Builder()
+//                    .appendQueryParameter("tIDs", txID)
+//                    .appendQueryParameter("domain", merchantId)
+//                    .appendQueryParameter("skey", sKey)
+//                    .appendQueryParameter("url", "")
+//                    .appendQueryParameter("type", "0");
 
             return postRequest(uri, builder);
         } catch (JSONException e) {
